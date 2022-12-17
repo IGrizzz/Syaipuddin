3335db1289411aa7f00149224e7b084f234e692d
IGrizzz <muda3332@gmail.com>
2022-12-17 22:37:10 +0700

    Adding responsive to the work comps.

--- src/Work.js
+++ src/Work.js
@@ -9,10 +9,17 @@ background : url(${boxWork}) no-repeat;
 background-size : 595px 506px;
 display : flex;
 flex-direction : column;
+@media only screen and (max-width : 580px){
+    width : 343px;
+    height : 290px;
+    background-size: 343px 290px;
+}
 `
 const WorkText = styled.h1`
 font-size: 36px;
 padding-left: 140px;
+@media only screen and (max-width : 580px){
+    font-size : 20px
 }
 `
 const Work1 = styled.div`
@@ -24,21 +31,41 @@ padding-left 60px;
 const Year = styled.p`
 font-size : 20px;
 height:6px;
+@media only screen and (max-width : 580px){
+    font-size : 12px;
+}
 `
 
 const Role = styled.p`
 font-size : 20px;
 height : 6px;
-font-style: italic
+font-style: italic;
+@media only screen and (max-width : 580px){
+    font-size : 12px;
+}
+`
+const Role1 = styled(Role)`
+font-size : 20px;
+height : 6px;
+@media only screen and (max-width : 580px){
+    font-size : 12px;
+}
 `
 
 const Place = styled.p`
 font-size : 36px;
 height : 20px;
+@media only screen and (max-width : 580px){
+    font-size : 20px;
+    height : 8px;
+}
 `
 
 const Place1 = styled(Place)`
-height : 70px;`
+height : 70px;
+@media only screen and (max-width : 580px){
+    height : 32px;
+}`
 
 function Work (){
 return(
@@ -53,7 +80,7 @@ return(
             <Work2>
                 <Place1>Computational Physics Laboratory</Place1>
                 <Role>Universitas Sumatera Utara</Role>
-                <Role>Laboratory Coordinator</Role>
+                <Role1>Laboratory Coordinator</Role1>
                 <Year>Aug. 2021 - Present</Year>
             </Work2>
         </Box>
